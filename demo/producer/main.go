7ca3b5ebbae5d7d8e1e3ffbc8ab4cf3f8d867eee
Will Charczuk <wcharczuk@users.noreply.github.com>
2025-05-24 16:25:17 -0700

    errant v1 aws sdk go thing

--- demo/producer/main.go
+++ demo/producer/main.go
@@ -7,11 +7,11 @@ import (
 	"os"
 	"sync/atomic"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/spf13/pflag"
 )
 
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module sqslite
 go 1.23.1
 
 require (
-	github.com/aws/aws-sdk-go v1.55.6
 	github.com/aws/aws-sdk-go-v2 v1.36.3
 	github.com/aws/aws-sdk-go-v2/config v1.29.14
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.67
