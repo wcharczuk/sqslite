0a4bbf004fd5921a0acd2eb75bea3bf6449ff3fb
Will Charczuk <wcharczuk@users.noreply.github.com>
2025-05-26 07:22:25 -0700

    more tests

--- pkg/sqslite/safe_set.go
+++ pkg/sqslite/safe_set.go
@@ -10,6 +10,13 @@ type SafeSet[T comparable] struct {
 	storage   map[T]struct{}
 }
 
+func (s *SafeSet[T]) Len() (output int) {
+	s.storageMu.Lock()
+	output = len(s.storage)
+	s.storageMu.Unlock()
+	return
+}
+
 func (s *SafeSet[T]) Add(v T) {
 	s.storageMu.Lock()
 	defer s.storageMu.Unlock()
@@ -23,18 +30,20 @@ func (s *SafeSet[T]) Has(v T) (ok bool) {
 	return
 }
 
-// Consume returns an iterator that yields element of the set
-// and purges the set once the iterator returns.
 func (s *SafeSet[T]) Consume() iter.Seq[T] {
 	return func(yield func(T) bool) {
 		s.storageMu.Lock()
 		defer s.storageMu.Unlock()
+		var toDelete []T
 		for v := range s.storage {
 			if !yield(v) {
 				return
 			}
+			toDelete = append(toDelete, v)
 		}
-		s.storage = make(map[T]struct{})
+		for _, v := range toDelete {
+			delete(s.storage, v)
+		}
 	}
 
 }
